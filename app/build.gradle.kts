809df6a183d211a513abe0a42fc821be304189cc
Pierce Goulimis <piercemg2013@gmail.com>
2024-10-26 14:28:20 -0400

    - Set up data models, database, and main activity layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,4 +38,12 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+
+    //Add dependencies
+    implementation ("androidx.room:room-runtime:2.4.3")
+    annotationProcessor ("androidx.room:room-compiler:2.4.3")
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
+
 }
